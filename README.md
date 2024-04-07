c11b4f9a11663029dac341cc92ded53d767874c3
luoyihao <13324612605@qq.com>
2024-04-07 23:25:00 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# 基于SpringBoot3+Vue3整合开发学生信息管理系统
+
+> 后端项目gitHub源码地址：https://github.com/seasonl2014/student-mangement-system
+>
+> 前端项目gitHub源码地址：https://github.com/seasonl2014/student-mangement-system-web
+>
+> 欢迎给个star鼓励一下~
+
+## 系统功能结构
+
+本系统主要有两种角色，分别管理员角色和普通用户角色，其中：
+
+管理员角色主要功能有：后台首页展示、个人信息展示和修改、用户管理、角色管理、班级管理、学生管理、课程管理、教师管理、班级科目成绩管理、班级科目成绩统计、班级科目对比统计等功能，如图所示：
+
+
+普通用户角色主要功能有：后台首页展示、个人信息展示和修改、班级科目成绩管理、班级科目成绩统计、班级科目对比统计等功能，如图所示：
+
+
+
+
+
+
+## 技术栈
+
+1. web框架：SpringBoot3.X
+2. 数据库持久框架：Sping Data JPA
+3. 数据库：MySql
+4. 项目构建工具：Maven、vite
+5. 前端框架：Vue3.X、element plus、pina、axios、Vue Router
+6. 数据图表：ECharts
+
+## 数据库表
+
+本项目由七张表，分别如下：
+
+1、用户表（sys_user）
+
+2、角色表（sys_role）
+
+3、教师表（s_teacher）
+
+4、班级表（s_grade_class）
+
+5、学生表（s_student）
+
+6、课程表（s_course）
+
+7、学生成绩表（s_student_score）
+
+
+
+新增代码
+
+
+
+
+
+
+
+
---
+++ src/views/login/Login.vue
@@ -0,0 +1,91 @@
+<template>
+<div class="login-container">
+  <div class="login-box">
+    <!--左边背景图 start-->
+    <!--左边背景图 end-->
+    <!--登录表单 start-->
+    <div class="login-form">
+      <!--标题和logo start-->
+      <div class="login-title">
+        <h2 class="title">学生信息管理系统</h2>
+      </div>
+      <!--标题和logo end-->
+      <!--登录表单组件 start-->
+       <LoginForm/>
+      <!--登录表单组件 end-->
+    </div>
+    <!--登录表单 end-->
+  </div>
+</div>
+</template>
+
+<script setup lang="ts">
+import LoginForm from './components/LoginForm.vue'
+</script>
+
+<style scoped>
+.login-container {
+  background-color: #f0f2f5;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 25px 25px;
+  box-sizing: border-box;
+  /* 如果需要固定宽度和高度，请添加下面的样式 */
+  width: 1000px;
+  height: 700px;
+}
+.login-box {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-color: #fffc;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.login-left{
+  width: 50%;
+}
+.login-left img{
+  width: 100%;
+  max-width: 900px;
+}
+
+.login-form{
+  max-width: 480px;
+  width: 50%;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 0 2px 12px 0 rgb(0 0 0 / 10%);
+  box-sizing: border-box;
+}
+
+.login-title{
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-bottom: 30px;
+}
+
+.login-title .title{
+  margin: 0;
+  font-size: 30px;
+  white-space: nowrap;
+}
+.login-title .icon{
+  width: 60px;
+}
+
+::v-deep(.el-input__inner){
+  height: 40px;
+}
+
+</style>
